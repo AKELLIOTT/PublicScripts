62174561195f51a7d5a090a9b787a2ff0f269d25
AKELLIOTT <alistairkelliott@gmail.com>
2026-01-19 13:16:43 +1000

    Create Disable UAC and Install Connectwise.ps1
    Add Script for Disable UAC to Install Connectwise

---
+++ Disable UAC and Install Connectwise.ps1
@@ -0,0 +1,10 @@
+﻿Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLUA' -Value '0'
+Get-MpPreference | Format-Table PUAProtection
+Set-MpPreference -PUAProtection Disabled
+
+Reboot 
+Install Connectwise 
+
+Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System' -Name 'EnableLUA' -Value '1'
+Set-MpPreference -PUAProtection Enabled
+Get-MpPreference | Format-Table PUAProtection
